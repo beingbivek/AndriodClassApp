f53751b26c39664094d4455a64c600358ceaa0ec
Bivek Thapa <beingbivek@gmail.com>
2025-12-16 19:31:35 +0545

    adding UI icons and runtime livedata(observestate) dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,30 +43,33 @@ android {
 }
 
 dependencies {
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+
+    // Compose BOM
     implementation(platform(libs.androidx.compose.bom))
+
+    // Compose
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation(libs.firebase.auth)
-    implementation(libs.firebase.database)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    implementation(libs.androidx.compose.icons)   // ← Icons.Filled now FIXED
+
     debugImplementation(libs.androidx.compose.ui.tooling)
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 
-    // Google Maps (Android + Compose)
+    // Firebase
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.database)
+
+    implementation("androidx.compose.runtime:runtime-livedata")
+
+    // Google Maps
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("com.google.android.gms:play-services-location:21.3.0")
-    implementation("com.google.maps.android:maps-compose:2.14.0") // Maps Compose
-
-    // (Optional) Firebase Realtime DB for live truck positions later
-    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
-    implementation("com.google.firebase:firebase-database-ktx")
+    implementation("com.google.maps.android:maps-compose:2.14.0")
 }
