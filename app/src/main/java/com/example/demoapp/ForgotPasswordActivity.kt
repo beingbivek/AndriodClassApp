66f5bfb77b8437fc2e950e9693b7a5e3d801f681
Bivek Thapa <beingbivek@gmail.com>
2025-12-16 19:34:47 +0545

    forgot password activity design with funcionality added

--- app/src/main/java/com/example/demoapp/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/demoapp/ForgotPasswordActivity.kt
@@ -1,18 +1,50 @@
 package com.example.demoapp
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.demoapp.repository.UserRepo
+import com.example.demoapp.repository.UserRepoImpl
+import com.example.demoapp.ui.theme.Blue
 import com.example.demoapp.ui.theme.DemoAppTheme
+import com.example.demoapp.ui.theme.White
+import com.example.demoapp.viewmodel.UserViewModel
 
 class ForgotPasswordActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,14 +56,84 @@ class ForgotPasswordActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ForgotPasswordBody(){
-    Scaffold() {
+    var email by remember { mutableStateOf("") }
+    val userViewModel= remember{ UserViewModel(UserRepoImpl()) }
+    val context = LocalContext.current
+    val activity = context as Activity
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Blue,
+                    titleContentColor = White,
+                    navigationIconContentColor = White,
+                    actionIconContentColor = White
+                ),
+                title = { Text("Forgot Password") },
+                navigationIcon = {
+                    IconButton(onClick = {
+                        activity.finish()
+                    }) {
+                        Icon(
+                            painter = painterResource(R.drawable.baseline_arrow_back_ios_24),
+                            contentDescription = null
+                        )
+                    }
+                }
+            )
+        },
+    ) {
         paddingValues ->
         Column(
             Modifier.fillMaxSize().padding(paddingValues)
         ) {
+            OutlinedTextField(
+                value = email,
+                onValueChange = { data ->
+                    email = data
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 10.dp),
+                placeholder = {Text("abc@gmail.com")},
+                shape = RoundedCornerShape(15.dp),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = White,
+                    unfocusedContainerColor = Blue.copy(0.01f),
+                    focusedIndicatorColor = Blue,
+                    unfocusedIndicatorColor = Color.Black.copy(0.25f)
+                )
+            )
 
+            Spacer(Modifier.height(20.dp))
+
+            Button(onClick = {
+                if(email != ""){
+                    userViewModel.forgetPassword(email){
+                        success,msg->
+                        if(success){
+                            Toast.makeText(context,msg,Toast.LENGTH_SHORT)
+                            activity.finish()
+                        }else{
+                            Toast.makeText(context,msg,Toast.LENGTH_SHORT)
+                        }
+                    }
+                } else {
+                    Toast.makeText(context,"Incorrect Email",Toast.LENGTH_SHORT)
+                }
+            },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 10.dp)
+                    .height(50.dp),
+                shape = RoundedCornerShape(10.dp),
+                elevation = ButtonDefaults.buttonElevation(defaultElevation = 15.dp)) {
+                Text("Send Verify Link")
+            }
         }
     }
 }
